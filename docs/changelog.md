142d9ac7fe90bb06c8d1cf3164458edcbbe0f33d
nsheff <nsheff@users.noreply.github.com>
2022-01-25 16:20:26 -0500

    fix 2to3, version to 0.1.4

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [0.1.4] - 2022-01-25
+### Fixed
+- removed use_2to3 for compatibility with setuptools >= 58
+
 ## [0.1.3] - 2019-11-01
 ### Added
 - distribute license with the package
--- setup.py
+++ setup.py
@@ -11,20 +11,11 @@ def read_reqs(reqs_name):
     with open(depsfile, 'r') as f:
         return [l.strip() for l in f if l.strip()]
 
-
-extra = {"use_2to3": True} if sys.version_info >= (3, ) else {}
-
 with open(os.path.join(PKG, "_version.py"), 'r') as versionfile:
     version = versionfile.readline().split()[-1].strip("\"'\n")
 
-# Handle the pypi README (long description) formatting.
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file("README.md", 'rst')
-    print("Pandoc conversion succeeded")
-except(IOError, ImportError, OSError):
-    print("Warning: pandoc conversion failed!")
-    long_description = open("README.md").read()
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
     name=PKG,
@@ -48,6 +39,5 @@ setup(
     include_package_data=True,
     test_suite="tests",
     tests_require=read_reqs("dev"),
-    setup_requires=(["pytest-runner"] if {"test", "pytest", "ptr"} & set(sys.argv) else []),
-    **extra
+    setup_requires=(["pytest-runner"] if {"test", "pytest", "ptr"} & set(sys.argv) else [])
 )
